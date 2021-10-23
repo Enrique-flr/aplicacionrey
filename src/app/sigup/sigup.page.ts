29ed1119bb1592beb02b3a89089b734e69b47b33
Enrique <e.floresss@outlook.es>
2021-10-23 10:44:22 -0500

    style de SigUp

--- src/app/sigup/sigup.page.ts
+++ src/app/sigup/sigup.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sigup',
   templateUrl: './sigup.page.html',
-  styleUrls: ['./sigup.page.scss'],
+  styleUrls: ['../login/login.page.scss'],
 })
 export class SigupPage implements OnInit {
 
