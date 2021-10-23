e4623270e2e043ed672b261e6cbd45b070420573
Enrique <e.floresss@outlook.es>
2021-10-22 19:02:05 -0500

    Creacion de las vistas login y sigup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'sigup',
+    loadChildren: () => import('./sigup/sigup.module').then( m => m.SigupPageModule)
   },
 ];
 
